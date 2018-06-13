b9aea19a29367e8c95efda2cd342ef5cb0c22777
Unknown <19liukat@elmbrookstudents.org>
2018-06-13 14:31:08 -0500

    Read Text File

--- FileIO/Program.cs
+++ FileIO/Program.cs
@@ -1,20 +1,22 @@
 ﻿using System;
 using System.IO;
+using System.Reflection;
+
 namespace FileIO
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-
-            Console.WriteLine("Enter the file name");
-            string[] lines = System.IO.File.ReadAllLines(@Console.ReadLine());
+            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            path += "//Text.txt";
+            string[] lines = System.IO.File.ReadAllLines(path);
             int[] numbers = new int[lines.Length];
             for (int i = 0; i < lines.Length; i++)
             {
                 numbers[i] = int.Parse(lines[i]);
             }
+            Console.WriteLine(numbers[6]);
             //numbers = final array w/ int
             Console.ReadKey();
 			}
